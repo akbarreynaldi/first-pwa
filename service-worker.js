f029c386c003de03133b23735d8d344b56f6a687
akbarreynaldi <akbarreynaldi77@gmail.com>
2020-06-20 20:20:53 +0700

    menambahkan fungsi untuk menggunakan aset dari cache

--- service-worker.js
+++ service-worker.js
@@ -18,3 +18,22 @@ self.addEventListener("install", function(event) {
         })
     );
 });
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches
+        .match(event.request, { cacheName: CACHE_NAME })
+        .then(function(response) {
+            if (response) {
+                console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+                return response;
+            }
+
+            console.log(
+                "ServiceWorker: Memuat aset dari server: ",
+                event.request.url
+            );
+            return fetch(event.request);
+        })
+    );
+});
